bff1b9705f65b5a9e37d8e8214d9fe62ec225461
root <root@svm8.llnl.gov>
2018-07-30 11:25:18 -0700

    Adding these files

---
+++ manifests/projectname.pp
@@ -0,0 +1,30 @@
+node "svm8.llnl.gov" {
+
+file { '/root/example_file.txt':
+    ensure => "file",
+    owner  => "root",
+    group  => "root",
+    mode   => "700",
+    content => "Congratulations!
+Puppet has created this file.
+",}
+
+} # End node svm8.llnl.gov
+
+file { '/etc/hosts':
+    source => "/etc/puppet/manifests/hosts",
+    mode => "644",
+    owner => "root",
+    group => "root",
+}
+
+file { '/etc/resolv.conf':
+    source => "/etc/puppet/manifests/resolv.conf",
+    mode => "644",
+    owner => "root",
+    group => "root",
+}
+
+package { 'git':
+    ensure => "latest",
+}
